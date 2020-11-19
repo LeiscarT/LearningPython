5cb5e63cb8657967be26133d3eef1b40cc90586e
LeiscarT <leiscartp29@gmail.com>
2020-11-18 21:27:59 -0400

    Exersice #1 Complete (Twinkle Twinkle)

---
+++ 120 exercises/exercise_#1.py
@@ -0,0 +1,1 @@
+print("Twinkle, twinkle, little star, \n How I wonder what you are! \n\t Up above the world so high, \n\t Like a diamond in the sky. \n Twinkle, twinkle, little star, \n\t How i wonder what you are")
