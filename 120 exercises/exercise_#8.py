040cde0adaef5ea813cbec3fedb4974ff97dd202
LeiscarT <leiscartp29@gmail.com>
2020-11-18 22:08:35 -0400

    [complete] exercise #8

---
+++ 120 exercises/exercise_#8.py
@@ -0,0 +1,2 @@
+listaDeColores = ["Rojo", "Azul", "Amarillo", "Verde", "Negro"]
+print("El primer color en la lista es " + repr(listaDeColores[0]) + " y el ultimo color en la lista es " + repr(listaDeColores[-1]))
