8bba26994f0c0a6c3c2077866a3b89834d170043
LeiscarT <leiscartp29@gmail.com>
2020-11-21 01:39:23 -0400

    [complete] exercise #20

---
+++ 120 exercises/[exercise] #20.py
@@ -0,0 +1,12 @@
+
+def multiplicaString (str,n):
+
+result = " "
+for i in range (n):
+    result = result + str
+return result
+
+
+print(multiplicaString('hola', 3))
+print(multiplicaString('adios', 5))
+
