4820f8826a2aa2fa96cd16d220c5a07349b4947f
LeiscarT <leiscartp29@gmail.com>
2020-11-21 10:29:23 -0400

    [complete] some exercises

---
+++ 120 exercises/[exercise] #51.py
@@ -0,0 +1,12 @@
+def no_notes(a):
+  Q = [500, 200, 100, 50, 20, 10]
+  x = 0
+  for i in range(6):
+    q = Q[i]
+    x += int(a / q)
+    a = int(a % q)
+  if a > 0:
+    x = -1
+  return x
+print(no_notes(880))
+print(no_notes(1000))
---
+++ 120 exercises/[exercise] #52.py
@@ -0,0 +1,7 @@
+def new_seq(n):
+    if n==1 or n==2 or n==3 or n==4:
+        return 1
+    return new_seq(n-1) + new_seq(n-2) + new_seq(n-3) + new_seq(n-4)
+print(new_seq(5))
+print(new_seq(6))
+print(new_seq(7))
