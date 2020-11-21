f0f45dbc0aa14031c1968ba3fc971b7b099cf5f3
LeiscarT <leiscartp29@gmail.com>
2020-11-21 09:21:31 -0400

    ÃÃ[complete] exercise #25

---
+++ 120 exercises/[exercise] #25.py
@@ -0,0 +1,9 @@
+def is_group_member (group_data, n):
+
+for value in group_data:
+    if n == value:
+    return True
+return False
+
+print(is_group_member([1,5,6,3],3))
+print(is_group_member[5,8,3],-1)
